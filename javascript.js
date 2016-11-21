25ea9c06166fcc707b24193a76c9fe421c07fd90
Braden Amorozo <braden808@gmail.com>
2016-11-21 08:45:09 -1000

    update

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 var answer;
 
 function calculator(x,y,z){
-	if (x+y)
+  if (x == "add"){
+	answer = (y + z);
+    console.log(answer);
+  } else if (x == "subtract") {
+  	answer = (x - z);
+  	console.log(answer);
+  } else if (x == "multiply") {
+  	answer = (x * z);
+  	console.log(answer);
+  } else if (x == "divide") {
+  	answer = (x / z);
+  	console.log(answer);
+  } else {
+  	console.log ("you failed math")
+  }
 }
+
+calculator ("multiply", 7,9);
